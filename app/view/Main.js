94f5905eeab671945465fdb581a5ddacc4fe38eb
Eric Betts <bettse@gmail.com>
2012-04-10 22:48:52 -0700

    move carousel up to first level, implicitly hide tabpanel and list

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,17 +1,7 @@
 Ext.define("JRSS.view.Main", {
-    extend: 'Ext.tab.Panel',
-    requires: ['Ext.TitleBar', 'Ext.carousel.Carousel'],
-
+    extend: 'Ext.carousel.Carousel',
+    requires: ['Ext.carousel.Carousel'],
     config: {
-        tabBarPosition: 'bottom',
-        items: [{
-            xtype: 'blog'
-        },{
-            title: 'Carousel',
-            iconCls: 'star',
-
-            xtype: 'carousel'
-
-        }]
+        xtype: 'carousel'
     }
 });
