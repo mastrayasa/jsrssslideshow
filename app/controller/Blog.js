546282dd62eb65623954c310c9e6df1e89a8ac72
Eric Betts <bettse@gmail.com>
2012-04-10 22:46:10 -0700

    Create carousel panels from store, advance every 30seconds, reload store every 15minutes

--- app/controller/Blog.js
+++ app/controller/Blog.js
@@ -7,7 +7,7 @@ Ext.define('JRSS.controller.Blog', {
 
 
         refs: {
-
+            carousel: 'carousel'
         },
         control: {
 
@@ -16,7 +16,32 @@ Ext.define('JRSS.controller.Blog', {
 
     //called when the Application is launched, remove if not needed
     launch: function(app) {
-        console.log("Launched!");
-        console.log(app);
+        var me = this;
+
+        //Transition to next slide
+        var transitionTime = 30;
+        Ext.Function.defer(function(){
+            me.getCarousel().setActiveItem(me.getCarousel().getActiveIndex()+1);
+        }, transitionTime * 1000);
+
+        //Reload feed
+        var reloadTime = 15;
+        Ext.Function.defer(function(){
+            Ext.getStore('BlogEntries').load();
+        }, reloadTime * 60 * 1000);
+
+        //At first and each store load, create the carousel panels and set to the first
+        Ext.getStore('BlogEntries').on('load', function(store, records, successful, operation, eOpts) {
+            var items = [];
+            Ext.each(records, function(entry) {
+                items.push({
+                    xtype: 'panel',
+                    html: entry.get('content')
+                });
+            });
+            me.getCarousel().setItems(items);
+            me.getCarousel().setActiveItem(0);
+        });
+
     }
 });
